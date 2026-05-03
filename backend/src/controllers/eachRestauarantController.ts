a0f7609bfbeb99de98be766fb29c99dc3acb79fc
Krish Shah <krishcshah09@gmail.com>
2026-05-04 02:52:09 +0530

    added put each restaurant

---
+++ backend/src/controllers/eachRestauarantController.ts
@@ -0,0 +1,56 @@
+import type {Request,Response} from 'express'
+import Restaurant from '../models/restaurant'
+import mongoose from 'mongoose'
+
+export async function getEachRestaurant(req:Request<{id:string}>,res:Response){
+    const {id}=req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:"invalid id"})
+    }
+    try{
+        const restaurant=await Restaurant.findById(id).lean()
+        if(!restaurant){
+            return res.status(404).json({error:"Restaurant not found"})
+        }
+        const sum=restaurant.sumRating
+        const count=restaurant.ratings.length
+        const avg=count===0?0:sum/count
+        const updatedRestaurants={
+            ...restaurant,
+            avgRating:avg
+        }
+        return res.status(200).json({restaurant:updatedRestaurants})
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:"Internal server error"})
+    }
+}
+export async function putEachRestaurant(req:Request<{id:string},unknown,{
+    name?:string,
+    description?:string,
+    categories?:string[],
+    address?:string,
+    coordinates?:[number,number]
+}>,res:Response){
+    const {id}=req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({error:"invalid id"})
+    } 
+    let {name,description,categories,address,coordinates}=req.body
+    try{
+        const restaurant=await Restaurant.findById(id)
+        if(!restaurant){
+            return res.status(404).json({error:"Restaurant not found"})
+        }
+        restaurant.name=name?name.trim()?name.trim():restaurant.name:restaurant.name
+        restaurant.description=description?description.trim()?description.trim():restaurant.description:restaurant.description
+        restaurant.categories=categories?categories.length>0?categories:restaurant.categories:restaurant.categories
+        restaurant.address=address?address.trim()?address.trim():restaurant.address:restaurant.address
+        restaurant.location.coordinates=coordinates?coordinates:restaurant.location.coordinates
+        await restaurant.save()
+        return res.status(200).json({ restaurant })
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:"Internal server error"})
+    }   
+}
--- backend/src/routers/restaurantsRouter.ts
+++ backend/src/routers/restaurantsRouter.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import type {Router} from 'express'
 import {postRestaurantsController,getRestaurantsController,getRestaurantsNearController} from '../controllers/restaurantsController'
 import {postRatingController} from '../controllers/ratingController'
-import {getEachRestaurant} from '../controllers/getEachRestauarant'
+import {getEachRestaurant,putEachRestaurant} from '../controllers/eachRestauarantController'
 
 const router:Router=express.Router()
 
@@ -11,5 +11,6 @@ router.get('/',getRestaurantsController)
 router.get('/near',getRestaurantsNearController)
 router.post('/:id/rating',postRatingController)
 router.get('/:id',getEachRestaurant)
+router.put('/:id',putEachRestaurant)
 
 export default router
