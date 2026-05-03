0b74229ac6f2df06df0b6286518ce2d3da4e80f4
Krish Shah <krishcshah09@gmail.com>
2026-05-04 02:23:45 +0530

    added get each restaurant contorller

--- backend/src/routers/restaurantsRouter.ts
+++ backend/src/routers/restaurantsRouter.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import type {Router} from 'express'
 import {postRestaurantsController,getRestaurantsController,getRestaurantsNearController} from '../controllers/restaurantsController'
 import {postRatingController} from '../controllers/ratingController'
+import {getEachRestaurant} from '../controllers/getEachRestauarant'
 
 const router:Router=express.Router()
 
@@ -9,5 +10,6 @@ router.post('/',postRestaurantsController)
 router.get('/',getRestaurantsController)
 router.get('/near',getRestaurantsNearController)
 router.post('/:id/rating',postRatingController)
+router.get('/:id',getEachRestaurant)
 
 export default router
