8934b6a8e7c687e26838231f7b4edeccaba5febf
Krish Shah <krishcshah09@gmail.com>
2026-05-04 22:26:56 +0530

    protected routes with isAdmin

--- backend/src/routers/restaurantsRouter.ts
+++ backend/src/routers/restaurantsRouter.ts
@@ -3,15 +3,16 @@ import type {Router} from 'express'
 import {postRestaurantsController,getRestaurantsController,getRestaurantsNearController} from '../controllers/restaurantsController'
 import {postRatingController} from '../controllers/ratingController'
 import {getEachRestaurant,putEachRestaurant,deleteEachRestaurant} from '../controllers/eachRestauarantController'
+import isAdmin from '../middleware/isAdmin'
 
 const router:Router=express.Router()
 
-router.post('/',postRestaurantsController)
+router.post('/',isAdmin,postRestaurantsController)
 router.get('/',getRestaurantsController)
 router.get('/near',getRestaurantsNearController)
 router.post('/:id/rating',postRatingController)
 router.get('/:id',getEachRestaurant)
-router.put('/:id',putEachRestaurant)
-router.delete("/:id",deleteEachRestaurant)
+router.put('/:id',isAdmin,putEachRestaurant)
+router.delete("/:id",isAdmin,deleteEachRestaurant)
 
 export default router
